48545ebc46ebddc011c0ee3f034184d73a9e0be9
Adam Lancaster <lancaster28@hotmail.com>
2017-05-04 11:32:04 +0100

    drastically simplifies the navbar, uses component state instead of redux. Implements Drawer

--- src/components/navigation/navbar/Navbar.jsx
+++ src/components/navigation/navbar/Navbar.jsx
@@ -1,42 +1,77 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Radium from 'radium';
-import { PropTypes } from 'prop-types';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import NavigationMenuIcon from 'material-ui/svg-icons/navigation/menu';
+import MenuItem from 'material-ui/MenuItem';
 import { Toolbar, ToolbarTitle, ToolbarGroup } from 'material-ui/Toolbar';
+import Drawer from 'material-ui/Drawer';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import { white, steelBlue } from '../../../styles/colors';
-import MainNavigationPopover from '../mainNavigationPopover/MainNavigationPopover';
 
 injectTapEventPlugin();
 
-export const Navbar = props => (
-  <div>
-    <Toolbar style={{ backgroundColor: steelBlue }}>
-      <ToolbarGroup style={{ width: '100%' }}>
-        <MainNavigationPopover
-          style={{ color: white }}
-        />
-        <ToolbarTitle
-          text={props.selectedOption.title}
-          style={{ color: white, margin: '0 auto' }}
-        />
-      </ToolbarGroup>
-    </Toolbar>
-  </div>
-);
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      options: [
+        { id: 1, title: 'Data Management', slug: 'data_management' },
+        { id: 2, title: 'Order Creation', slug: 'order_creation' },
+        { id: 3, title: 'Brand Product Catalogue', slug: 'brand_product_catalogue' },
+      ],
+      selectedOption: { id: 1, title: 'Data Management', slug: 'data_management' },
+    };
+  }
 
-Navbar.propTypes = {
-  selectedOption: PropTypes.shape({
-    id: PropTypes.int,
-    title: PropTypes.string,
-    slug: PropTypes.string,
-  }).isRequired,
-};
+  handleToggle() {
+    this.setState({ open: !this.state.open });
+  }
 
-function mapStateToProps(state) {
-  return {
-    selectedOption: state.mainNavPopover.selectedOption,
-  };
+  handleClose() {
+    this.setState({ open: false });
+  }
+
+  handleOptionTouchTap(option) {
+    this.setState({ selectedOption: option, open: false });
+  }
+
+  renderMenuItems() {
+    return this.state.options.map(option => (
+      <Link to={`/${option.slug}`} key={option.id} style={{ textDecoration: 'none' }}>
+        <MenuItem
+          key={option.id}
+          primaryText={option.title}
+          onTouchTap={() => this.handleOptionTouchTap(option)}
+        />
+      </Link>
+    ));
+  }
+
+  render() {
+    return (
+      <div>
+        <Toolbar style={{ backgroundColor: steelBlue }}>
+          <ToolbarGroup style={{ width: '100%' }}>
+            <NavigationMenuIcon
+              onTouchTap={() => this.handleToggle()}
+              style={{ color: white }}
+            />
+            <Drawer
+              docked={false}
+              width={250}
+              open={this.state.open}
+              onRequestChange={open => this.setState({ open })}
+            >
+              {this.renderMenuItems()}
+            </Drawer>
+            <ToolbarTitle
+              text={this.state.selectedOption.title}
+              style={{ color: white, margin: '0 auto' }}
+            />
+          </ToolbarGroup>
+        </Toolbar>
+      </div>
+    );
+  }
 }
 
-export default connect(mapStateToProps)(Radium(Navbar));
